ccd4b650b3e88eed1d84efe30e621c0ec77e0fa0
Kaito Kikuchi <kaitokikuchi@Kaitos-MacBook-Pro.local>
2019-06-01 22:32:08 +0900

    add some functions but add and delete functions do not work correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+int count = 0;
+List<Widget> _tasks = [];
 
 void main() => runApp(
       MaterialApp(debugShowCheckedModeBanner: false, home: AllTasksPage()),
@@ -11,12 +15,8 @@ class AllTasksPage extends StatefulWidget {
 }
 
 class _AllTasksPageState extends State<AllTasksPage> {
-  int _count = 0;
-
   @override
   Widget build(BuildContext context) {
-    List<Widget> _tasks = List.generate(_count, (int i) => TaskColumn());
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Task Management'),
@@ -28,8 +28,16 @@ class _AllTasksPageState extends State<AllTasksPage> {
             children: <Widget>[
               Expanded(
                 child: Container(
-                  child: ListView(
-                    children: _tasks,
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      _tasks.add(Task(
+                        index: index,
+                      ));
+
+                      return _tasks[index];
+                    },
+                    //_tasks[index],
+                    itemCount: count,
                   ),
                 ),
               ),
@@ -38,7 +46,7 @@ class _AllTasksPageState extends State<AllTasksPage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addNewTask,
+        onPressed: _addTask,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
@@ -46,48 +54,137 @@ class _AllTasksPageState extends State<AllTasksPage> {
     );
   }
 
-  void _addNewTask() {
+  void _addTask() {
     setState(() {
-      _count++;
+      count++;
+      //_tasks.add(Task());
     });
   }
 }
 
-class TaskColumn extends StatefulWidget {
-  TaskColumn({int count, Key key})
-      : this.count = count,
-        super(key: key);
-  final count;
+class Task extends StatefulWidget {
+  final int index;
+  const Task({this.index, Key key}) : super(key: key);
 
-  _TaskColumnState createState() => _TaskColumnState();
+  _TaskState createState() => _TaskState();
 }
 
-class _TaskColumnState extends State<TaskColumn> {
+class _TaskState extends State<Task> {
+  Color _taskColor = Colors.blueAccent;
+  bool _checked = false;
+  double _importance = 1;
+
+  void _toggleChecked() {
+    setState(() {
+      if (_checked) {
+        _checked = false;
+      } else {
+        _checked = true;
+      }
+    });
+  }
+
+  void _deleteItem(int i) {
+    setState(() {
+      _tasks.remove(_tasks[i]);
+      count--;
+    });
+  }
+
+  Future<Null> _displaySizeChangeDialog(int i) {
+    return showDialog<Null>(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.black,
+            actions: <Widget>[
+              Container(
+                padding: EdgeInsets.only(right: 50),
+                child: IconButton(
+                  icon: Icon(Icons.adjust),
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    setState(() {
+                      _taskColor = Colors.blueAccent;
+                      _importance = 1;
+                      Navigator.of(context).pop();
+                    });
+                  },
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(right: 55),
+                child: IconButton(
+                  icon: Icon(Icons.adjust),
+                  color: Colors.yellowAccent,
+                  onPressed: () {
+                    setState(() {
+                      _taskColor = Colors.yellowAccent;
+                      _importance = 1.5;
+                      Navigator.of(context).pop();
+                    });
+                  },
+                ),
+              ),
+              Container(
+                child: IconButton(
+                  icon: Icon(Icons.adjust),
+                  color: Colors.redAccent,
+                  onPressed: () {
+                    setState(() {
+                      _taskColor = Colors.redAccent;
+                      _importance = 2;
+                      Navigator.of(context).pop();
+                    });
+                  },
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
-        color: Colors.blueAccent,
+        color: _taskColor,
       ),
       margin: EdgeInsets.all(5),
-      height: 40,
+      height: 60 * _importance,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
+          Container(
+            //padding: EdgeInsets.only(left: 150),
+            child: IconButton(
+              onPressed: _toggleChecked,
+              icon: _checked
+                  ? Icon(Icons.check_box)
+                  : Icon(Icons.check_box_outline_blank),
+            ),
+          ),
           Expanded(
-            child: Container(
-              padding: EdgeInsets.only(left: 120),
-              child: Center(
-                child: Text('task'),
+            child: InkWell(
+              onLongPress: () {
+                _displaySizeChangeDialog(widget.index);
+              },
+              child: Container(
+                child: Center(
+                  child: Text('task'),
+                ),
               ),
             ),
           ),
           Container(
-            padding: EdgeInsets.only(left: 150),
+            padding: EdgeInsets.only(right: 10),
             child: IconButton(
-              onPressed: null,
               icon: Icon(Icons.clear),
+              onPressed: () {
+                _deleteItem(widget.index);
+              },
             ),
           ),
         ],
